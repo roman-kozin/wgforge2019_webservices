2294907bb7a953c7bde5e956ed8ffab634445787
Roman Kozin <r_kozin@wargaming.net>
2019-11-27 18:16:33 +0300

    first release

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+:$1$KTWkqEr6$ZdAGWPGbIRIqY3g8bbsbZ.:::admin,user
